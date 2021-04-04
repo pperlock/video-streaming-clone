fb447d8a10f6bd3719b8eda13cb41908155e11ad
Patti Perlock <pattiperlock@gmail.com>
2021-04-04 07:48:22 -0400

    Created nod project in root for deployment

--- client/src/actions/mainVideo.js
+++ client/src/actions/mainVideo.js
@@ -2,6 +2,8 @@ import {UPDATE_MAIN_STARTED, UPDATE_MAIN_SUCCESS, UPDATE_MAIN_FAILURE} from './t
 
 import axios from 'axios';
 
+const API_URL = process.env.NODE_ENV === "production" ? 'https://video-streaming-clone.herokuapp.com/': 'http://localhost:5000';
+
 export const updateMainVideoSuccess = video => ({
     type:UPDATE_MAIN_SUCCESS,
     payload:video
@@ -19,7 +21,7 @@ export const updateMainVideoFailure = error => ({
 export const updateMainVideo = (videoId)=> {
     return (dispatch, getState) =>{
         dispatch(updateMainVideoStarted());
-        axios.get("http://localhost:8080/videos/" + videoId)
+        axios.get(`${API_URL}videos/${videoId}`)
         .then(res=>{
             res.data !== "failed" ? dispatch(updateMainVideoSuccess(res.data)) : dispatch(updateMainVideoFailure("Video Not Found"));
         })
